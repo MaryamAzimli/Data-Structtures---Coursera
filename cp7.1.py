23e6efecd02dfb18fef67b3fc0d5b225c72f24a7
Maryam Azimli <91348821+MaryamAzimli@users.noreply.github.com>
2023-08-14 05:46:12 +0300

    Update cp7.1.py

--- cp7.1.py
+++ cp7.1.py
@@ -1,7 +1,7 @@
 # Use words.txt as the file name
 """fname = input("Enter file name: ")"""
 
-fh = open(r"C:\\Users\\Maryam Azimli\Desktop\\Coursera\\Python\\Data Structures\\cp7 assignments\\words.txt")
+fh = open(r"C:\\words.txt")
 
 for line in fh:
     line=line.rstrip()
